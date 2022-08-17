993968c0e94ebdeeccad1d7cbb5518eaf3b82b21
Gabriel Gutierrez <34143293+ggpgutierrez@users.noreply.github.com>
2022-08-17 10:58:45 -0300

    Delete tf directory

--- tf/eslint.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-node_modules/.bin/eslint --quiet \
-    --max-warnings 100 \
-    -o eslint-output.$1 \
-    -f $1 '*/**/*.{js,ts,tsx}' 
--- tf/tf-init.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-./tf/terraform init \
-    -no-color \
-    -input=false \
-    -backend-config="token=$TFE_CREDENTIALS" \
-    -var "SOURCE_PATH=$SOURCE_PATH" \
-    ./tf/
