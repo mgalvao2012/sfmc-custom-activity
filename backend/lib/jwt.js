c24a3b0db3c2826029946154214f2add7d2f5045
mgalvao2012 <mgalvao2012@gmail.com>
2022-08-19 19:00:33 -0300

    ajustes

--- backend/lib/jwt.js
+++ backend/lib/jwt.js
@@ -5,6 +5,7 @@ module.exports = (body, secret, cb) => {
 		return cb(new Error('invalid jwtdata'));
 	}
 
+	console.log(body.toString('utf8'));
 	require('jsonwebtoken').verify(body.toString('utf8'), secret, {
 		algorithm: 'HS256'
 	}, cb);
