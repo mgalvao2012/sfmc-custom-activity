5f5c5628cc51928832962fcfadba6da1e245327e
mgalvao2012 <mgalvao2012@gmail.com>
2022-08-22 19:45:53 -0300

    ajuste na chave do jwt

--- backend/lib/jwt.js
+++ backend/lib/jwt.js
@@ -5,7 +5,8 @@ module.exports = (body, secret, cb) => {
 		return cb(new Error('invalid jwtdata'));
 	}
 
-	console.log(body.toString('utf8'));
+	//console.log(body.toString('utf8'));
+	console.log("body => ", JSON.stringify(body));
 	require('jsonwebtoken').verify(body.toString('utf8'), secret, {
 		algorithm: 'HS256'
 	}, cb);
