58db2ec38f77484dc1d6190ad8bf0e290792493d
mgalvao2012 <mgalvao2012@gmail.com>
2022-08-23 13:52:15 -0300

    ajuste na chamada de api externa

--- backend/helper.js
+++ backend/helper.js
@@ -15,11 +15,13 @@ texto = (data) =>{
 
 reqWhatsapp = async (data) =>{
     return token.getAccessToken().then( (access_token) => {
-        console.log(`data ${data}`);
+        console.log("data:");
+        console.log(data);
+        // url: `https://nodejs-express-mysql-mgalvao.herokuapp.com/customers/${data[0]}`,
+
         axios({
             method: "post",
-            /* url: "https://webhook.site/4eb9a22c-8585-4fec-ab78-f98dda56b780", */
-            url: `https://nodejs-express-mysql-mgalvao.herokuapp.com/customers/${data[0]}`,
+            url: "https://webhook.site/4eb9a22c-8585-4fec-ab78-f98dda56b780",
             headers: {
                 'Content-Type': 'application/json'
             },
