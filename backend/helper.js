26fd77e455422c5342ee758d50d631e3f974b425
mgalvao2012 <mgalvao2012@gmail.com>
2022-08-23 11:57:07 -0300

    ajuste na chamada de api externa

--- backend/helper.js
+++ backend/helper.js
@@ -8,31 +8,23 @@ exports.populaMensagem = async (req) => {
 texto = (data) =>{
     let inArguments = data['inArguments'][0];
     var obj = {
-        "template":{
-           "nome":inArguments.template,
-           "bodyParameters": [inArguments.variables],
-           "buttonURL":inArguments.btnLink
-        },
-        "destino":inArguments.phoneNumber,
-        "origem": inArguments.origem
+        "bodyParameters": [inArguments.variables],
      }
      return obj;
 }
 
 reqWhatsapp = async (data) =>{
     return token.getAccessToken().then( (access_token) => {
-            console.log("access_token");
-            console.log(access_token);
+        console.log(`data ${data}`);
         axios({
             method: "post",
-            url: "https://webhook.site/4eb9a22c-8585-4fec-ab78-f98dda56b780",
+            /* url: "https://webhook.site/4eb9a22c-8585-4fec-ab78-f98dda56b780", */
+            url: `https://nodejs-express-mysql-mgalvao.herokuapp.com/customers/${data[0]}`,
             headers: {
                 'Content-Type': 'application/json'
-                /* 'access_token' : access_token */
             },
             data: data
-        }).then(resp =>{
-            
+        }).then(resp =>{            
             console.log(resp.data);
             return resp.data;
         }).catch(error =>{
